633c56b1f60edcb579488cb7a556aebbae9ed7cb
David Glick <dglick@gmail.com>
2009-12-13 00:02:25 -0800

    removed test file

--- foo.py
+++
@@ -1,6 +0,0 @@
-#!/opt/local/bin/python2.6
-import sys
-sys.path.append('/Users/davidg/ONENW/mr.igor')
-import mr.igor
-from pyflakes.scripts.pyflakes import checkPath
-checkPath('/Users/davidg/ONENW/mr.igor/bar.py')
