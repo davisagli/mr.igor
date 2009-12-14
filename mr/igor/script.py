3cfda893a2586e4122d7f26f2993a0da6799923b
David Glick <dglick@gmail.com>
2009-12-13 22:59:31 -0800

    add help

--- mr/igor/script.py
+++ mr/igor/script.py
@@ -4,7 +4,16 @@ import sys
 from pyflakes.scripts import pyflakes
 import mr.igor
 def main():
-    if sys.argv[1:2] == ['--print']:
-        sys.argv = sys.argv[0:1] + sys.argv[2:]
-        mr.igor.PRINT = True
-    pyflakes.main()
+    if len(sys.argv) == 1 or sys.argv[1:2] in (['-h'], ['--help']):
+        print """
+Igor records your imports and adds missing imports for names it recognizes.
+Usage: igor [--print] filename
+
+  --print  Causes Igor to write the modified file to stdout rather than
+           making changes inplace.
+"""
+    else:    
+        if sys.argv[1:2] == ['--print']:
+            sys.argv = sys.argv[0:1] + sys.argv[2:3]
+            mr.igor.PRINT = True
+        pyflakes.main()
